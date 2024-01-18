443ab64734561dfadcb6f89011590f18dc5b0307
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-01-18 13:30:55 -0600

    add pages, routes component

---
+++ src/components/routes.js
@@ -0,0 +1,32 @@
+import App from "../App";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Classes from "../pages/Classes";
+import Reports from "../pages/Reports";
+import ErrorPage from "../pages/ErrorPage";
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/classes",
+                element: <Classes />
+            },
+            {
+                path: "/reports",
+                element: <Reports />
+            }
+        ]
+    }
+]
