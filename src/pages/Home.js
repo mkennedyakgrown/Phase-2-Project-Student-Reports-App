dd0b28db2b4f2970775c30ffeefc539d2a8e31ff
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-02-14 18:00:24 -0600

    Add descriptive text to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { Outlet, useOutletContext } from "react-router-dom";
+import { Header } from "semantic-ui-react";
 
 function Home() {
   const context = useOutletContext();
@@ -6,7 +7,12 @@ function Home() {
   return (
     <>
       <main>
-        <h1>Welcome to the Classroom! Use the NavBar above to navigate.</h1>
+        <br />
+        <Header as="h1" textAlign="center">Welcome to the Classroom! Use the NavBar above to navigate.</Header>
+        <Header as="h2" textAlign="center" href="/admin">Admin:</Header>
+        <Header as="h4" textAlign="center">Add new students and search students' progress reports</Header>
+        <Header as="h2" textAlign="center" href="/reports">Reports:</Header>
+        <Header as="h4" textAlign="center">Write and save progress reports for students by class</Header>
       </main>
     </>
   );
